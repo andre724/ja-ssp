572d6c15160531eb632cd38d6162c32b1e3a079d
giovanne <andre.giovannetti.ext@nokia.com>
2021-09-28 12:48:38 -0300

    on models.py I changed some fields and added docstrings, and registered them on admin.py

--- ssp/social_scrapper/models.py
+++ ssp/social_scrapper/models.py
@@ -6,6 +6,9 @@ from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
 class SocialMedia(models.Model):
+    '''
+    Model for which social media bot is going to be used 
+    '''
     name = CharField(unique=True, null= False, blank= False,max_length=200)
     base_link= URLField(null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -14,14 +17,21 @@ class SocialMedia(models.Model):
 
 
 class SocialMediaAuthUser(models.Model):
+    '''
+    Model for the account's that the bots will be using
+    to log in their respective social medias.
+    '''
     username = CharField(null= False, blank= False,max_length=200)
     password = CharField(max_length=200)
     is_active = BooleanField(default=True)
-    social_media_id = ForeignKey(SocialMedia, int, null= False,blank=False)
+    social_media = ForeignKey(SocialMedia, null= False,blank=False,on_delete= models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class SocialMediaUser(models.Model):
+    '''
+    Model for the account that was scrapped
+    '''
     class Meta:
         unique_together = ('username', 'social_media_id')
     username= CharField(max_length=200, null=False,blank=False)
@@ -32,8 +42,12 @@ class SocialMediaUser(models.Model):
 
 
 class Post(models.Model):
+    '''
+    Model for the scrapped post from the SocialMediaUser
+    account
+    '''
     link = URLField(null=False,blank=False)
-    metadata= JSONField()
+    metadata= JSONField(null= True)
     status= CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
